004540e986d0fcd035340dda132caebe7cac0bac
Ken <itsyourboykenny@MacPro.attlocal.net>
2016-04-04 15:58:19 -0700

    Initial Commit

---
+++ CRC Algorithm/CRC Algorithm/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  CRC Algorithm
+//
+//  Created by Ken Ishiguro on 11/18/15.
+//  Copyright © 2015 Ken Ishiguro. All rights reserved.
+//
+
+#include <iostream>
+#include <utility>
+#include "CRC.h"
+
+int main(){
+    long long int fuck;
+    string test("Ren");
+    
+    cout << performCyclic(test) << endl;
+    
+    
+    
+}
